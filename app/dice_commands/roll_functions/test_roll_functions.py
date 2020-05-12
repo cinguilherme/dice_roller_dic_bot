3d647cc9c55d91a818a0279f4da5206e4b09a0e6
cinguilherme <cinguilherme@gmail.com>
2020-05-12 20:18:38 -0300

    add more tests to the get number of dices function

--- app/dice_commands/roll_functions/test_roll_functions.py
+++ app/dice_commands/roll_functions/test_roll_functions.py
@@ -29,3 +29,16 @@ class TestGetTypeOfDice(TestCase):
     def test_with_missing_type(self):
         dif = roll_functions.get_type_dices('10')
         assert(dif == 0)
+
+class TestGetNumberOfDices(TestCase):
+    def test_simple(self):
+        dif = roll_functions.get_num_dices('10d10')
+        assert(dif == 10)
+
+    def test_with_complete_input(self):
+        dif = roll_functions.get_num_dices('10d10>10')
+        assert(dif == 10)
+
+    def test_with_missing_type(self):
+        dif = roll_functions.get_num_dices('10')
+        assert(dif == 10)
