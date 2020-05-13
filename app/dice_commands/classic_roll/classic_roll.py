8de782b1b5c1de60544311d321fb8f931a2f205f
cinguilherme <cinguilherme@gmail.com>
2020-05-12 21:15:09 -0300

    making roll results less verbose

--- app/dice_commands/classic_roll/classic_roll.py
+++ app/dice_commands/classic_roll/classic_roll.py
@@ -5,22 +5,25 @@ def roll_damage(dice_pars, roll_functions):
         dice_pars).values()
     all_dices, success, crit_success, crit_failures = roll_functions.build_results(
         number_dices, type_dice, dificulty).values()
-    message = build_messages_of_atack(all_dices, success, crit_success, dificulty)
+    message = build_messages_of_damage(all_dices, success, crit_success, dificulty)
     return message
 
 def roll_dices(dices, roll_functions):
 
     number_dices, type_dice, dificulty = roll_functions.interpret_inp(
         dices).values()
+    
     all_dices, success, crit_success, crit_failures = roll_functions.build_results(
         number_dices, type_dice, dificulty).values()
+    
     success_calc = roll_functions.critical_balance(
         success, crit_success, crit_failures)
-    message = build_messages_str(all_dices, success, crit_success, crit_failures, success_calc, dificulty)
+    
+    message = build_messages_of_atack(all_dices, success, crit_success, crit_failures, success_calc, dificulty)
     return message
 
 
-def build_messages_of_atack(all_dices, success, crit_success, dificulty):
+def build_messages_of_damage(all_dices, success, crit_success, dificulty):
 
     message_list = build_messages_init(all_dices, dificulty, success)
     message_list.append(f'  results of success is {len(success)}')
@@ -28,12 +31,12 @@ def build_messages_of_atack(all_dices, success, crit_success, dificulty):
     return "\n".join(message_list)
 
 
-def build_messages_str(all_dices, success, crit_success, crit_failures, success_calc, dificulty):
+def build_messages_of_atack(all_dices, success, crit_success, crit_failures, success_calc, dificulty):
 
     message_list = build_messages_init(all_dices, dificulty, success)
     message_list.append(f'- you had {len(crit_failures)} critical failures - {crit_failures}')
     message_list.append(f'+ you had {len(crit_success)} critical success {crit_success}')
-    message_list.append(f'  results of success is {len(success) + len(crit_failures)}')
+    message_list.append(f'  results of success is {len(success) - len(crit_failures)}')
     message_list.append(f'+ results of successwith expecialization {success_calc}')
     return "\n".join(message_list)
 
@@ -41,6 +44,5 @@ def build_messages_str(all_dices, success, crit_success, crit_failures, success_
 def build_messages_init(all_dices, dificulty, success):
     message_list = []
     message_list.append(f'ok, rolling your dices!')
-    message_list.append(f"dices roll results! {all_dices}")
-    message_list.append(f'dificulty of {dificulty}, you had {len(success)} success + {success}')
+    message_list.append(f"all dices {all_dices}")
     return message_list
