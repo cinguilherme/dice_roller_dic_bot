e6be17de575a43f90f55c72b4a6dcd8b7142f73a
cinguilherme <cinguilherme@gmail.com>
2020-05-12 22:18:34 -0300

    updating format of exibition of results

--- app/app.py
+++ app/app.py
@@ -10,17 +10,6 @@ from discord.ext import commands
 client = commands.Bot(command_prefix='.')
 
 
-def my_latency():
-    return round(client.latency * 1000)
-
-
-def latency_commented(latency):
-    if latency > 120:
-        return ', I am very laggy..'
-    else:
-        return ', quite responsive'
-
-
 @client.event
 async def on_ready():
     print("The bot is ready to go.")
@@ -30,9 +19,10 @@ async def on_ready():
 async def roll_damage(ctx, *, dice_pars):
 
     messages = classic_roll.roll_damage(dice_pars, roll_functions)
+    general, success, success_crit = messages.values()
 
-    embed = make_embed(messages)
-    
+    embed = make_embed_messages_by_type(general=general, sucess=success, crits=success_crit)
+
     await ctx.send(embed=embed)
 
 
@@ -40,8 +30,9 @@ async def roll_damage(ctx, *, dice_pars):
 async def roll(ctx, *, dice_pars):
 
     messages = classic_roll.roll_dices(dice_pars, roll_functions)
-
-    embed = make_embed(messages)
+    general, success, crits, fails, balance, dificulty = messages.values()
+    
+    embed = make_embed_messages_by_type(general, success, crits, fails, balance)
 
     await ctx.send(embed=embed)
 
@@ -49,7 +40,6 @@ async def roll(ctx, *, dice_pars):
 @client.command(aliases=['.rs', 'rs', './rs', 'rolagem_atributo', 'stats_roll'])
 async def roll_without_least(ctx, *, dice_pars):
 
-    latency = my_latency()
     number_dices, type_dice, dificulty = roll_functions.interpret_inp(
         dice_pars).values()
     all_dices, success, crit_success, crit_failures = roll_functions.build_results(
@@ -65,7 +55,6 @@ async def roll_without_least(ctx, *, dice_pars):
 @client.command(aliases=['.rfx', 'rfx', './rfx', 'rolagem_fixo'])
 async def roll_plus_fix(ctx, *, dice_pars):
 
-    latency = my_latency()
     number_dices, type_dice, fix = roll_functions.interpre_plus_fix(
         dice_pars).values()
     dices = roll_functions.roll_n_dices(number_dices, type_dice)
@@ -78,19 +67,10 @@ async def basic_dice_roll_print(ctx, latency, all_dices):
     await ctx.send(f"here is your dice roll results! {all_dices}")
 
 
-def make_embed(messages):
+def make_embed_zip_messages(messages):
     retStr = str("""```diff\n""") + messages + """ ```"""
     
-    embed = discord.Embed(
-        title="Dice rolls", 
-        description='',
-        colour=discord.Color.dark_blue(),
-        type='rich')
-    
-    embed.set_footer(text='Dice roller footer')
-    embed.set_image(url='')
-    embed.set_thumbnail(url='')
-    embed.set_author(name='cinguilherme', icon_url='')
+    embed = create_base_embed()
 
     embed.add_field(name='general', value='general', inline=False)
     embed.add_field(name='sucesses', value='sss', inline=False)
@@ -99,6 +79,40 @@ def make_embed(messages):
     embed.add_field(name="results", value=retStr)
 
     return embed
+
+def make_embed_messages_by_type(general, sucess, crits, crit_failures='', balance='', bot=False):
+    
+    embed = create_base_embed()
+
+    general_str = str("""```diff\n""") + general + """ ```"""
+    embed.add_field(name='general', value=general_str, inline=False)
+    
+    sucess_str = str("""```diff\n+""") + sucess + """ ```"""
+    embed.add_field(name='sucesses', value=sucess_str, inline=False)
+    
+    crits_str = str("""```diff\n+""") + crits + """ ```"""
+    embed.add_field(name='critical', value=crits_str, inline=False)
+
+    if '' != crit_failures:
+        crit_failures_str = str("""```diff\n-""") + crit_failures + """ ```"""
+        embed.add_field(name='critical failures', value=crit_failures_str, inline=False)
+
+    return embed
+
+
+def create_base_embed():
+    embed = discord.Embed(
+        title="Dices results", 
+        description='results from rolls',
+        colour=discord.Color.dark_blue(),
+        type='rich')
+    
+    embed.set_footer(text='Dice roller footer')
+    embed.set_image(url='')
+    embed.set_thumbnail(url='')
+    embed.set_author(name='Dice Roller bot', icon_url='')
+    return embed
+
 
 with open('my_key.txt', 'r') as file:
     data = file.read().replace('\n', '')
--- app/dice_commands/classic_roll/classic_roll.py
+++ app/dice_commands/classic_roll/classic_roll.py
@@ -5,8 +5,11 @@ def roll_damage(dice_pars, roll_functions):
         dice_pars).values()
     all_dices, success, crit_success, crit_failures = roll_functions.build_results(
         number_dices, type_dice, dificulty).values()
-    message = build_messages_of_damage(all_dices, success, crit_success, dificulty)
-    return message
+    
+    message = build_messages_of_damage_as_string(all_dices, success, crit_success, dificulty)
+    message_object = build_messages_of_damage(all_dices, success, crit_success, dificulty)
+    
+    return message_object
 
 def roll_dices(dices, roll_functions):
 
@@ -19,19 +22,43 @@ def roll_dices(dices, roll_functions):
     success_calc = roll_functions.critical_balance(
         success, crit_success, crit_failures)
     
-    message = build_messages_of_atack(all_dices, success, crit_success, crit_failures, success_calc, dificulty)
-    return message
+    message = build_messages_of_atack_as_string(all_dices, success, crit_success, crit_failures, success_calc, dificulty)
+    messages_object = build_messages_of_atack(all_dices, success, crit_success, crit_failures, success_calc, dificulty)
 
+    return messages_object
 
 def build_messages_of_damage(all_dices, success, crit_success, dificulty):
+
+    messages = {
+        'general': f'all dices {all_dices}',
+        'success': f'results of success is {len(success)}',
+        'crits': f'results of successwith expecialization {len(success)+len(crit_success)}',
+        'dificulty': f'{dificulty}'
+    }
+
+    return messages
+
+def build_messages_of_damage_as_string(all_dices, success, crit_success, dificulty):
 
     message_list = build_messages_init(all_dices, dificulty, success)
     message_list.append(f'  results of success is {len(success)}')
     message_list.append(f'+ results of successwith expecialization {len(success)+len(crit_success)} ')
     return "\n".join(message_list)
 
-
 def build_messages_of_atack(all_dices, success, crit_success, crit_failures, success_calc, dificulty):
+    
+    messages = {
+        'general': f'all dices {all_dices}',
+        'success': f'results of success is {len(success)}',
+        'crits': f'results of successwith expecialization {crit_success}',
+        'fails': f'you had {len(crit_failures)} critical failures - {crit_failures}',
+        'balance': f'results of successwith expecialization {success_calc}',
+        'dificulty': f'{dificulty}'
+    }
+
+    return messages
+
+def build_messages_of_atack_as_string(all_dices, success, crit_success, crit_failures, success_calc, dificulty):
 
     message_list = build_messages_init(all_dices, dificulty, success)
     message_list.append(f'- you had {len(crit_failures)} critical failures - {crit_failures}')
@@ -40,6 +67,10 @@ def build_messages_of_atack(all_dices, success, crit_success, crit_failures, suc
     message_list.append(f'+ results of successwith expecialization {success_calc}')
     return "\n".join(message_list)
 
+def build_messages_init_object(all_dices):
+    return {
+        'general': f'all dices {all_dices}'
+    }
 
 def build_messages_init(all_dices, dificulty, success):
     message_list = []
