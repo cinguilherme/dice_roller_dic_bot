721779e3580a9d5a40f5aa1a2039a777a8083929
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 10:40:29 -0300

    house keeping on the embed file, try to simplify and display results according with final result

--- app/discord_embed_creator.py
+++ app/discord_embed_creator.py
@@ -3,12 +3,12 @@ import discord
 
 def build_simple_embed_discord_message(general, simple_output, bot=False):
     print(general, simple_output)
-    embed = create_simpler_embed()
+    embed = create_result_success_embed()
 
-    sucess_str = general_information(general)
+    sucess_str = neutral_text(general)
     embed.add_field(name='general', value=sucess_str, inline=False)
 
-    simple = general_information(simple_output)
+    simple = neutral_text(simple_output)
     embed.add_field(name='results', value=simple, inline=False)
 
     return embed
@@ -19,50 +19,30 @@ def build_embed_discord_message(general, sucess, crits, crit_failures='',
 
     embed = create_base_embed()
 
-    general_str = general_information(general)
+    general_str = neutral_text(general)
     embed.add_field(name='general', value=general_str, inline=False)
 
     # display negative crits if any
     if '' != crit_failures:
-        crit_failures_str = negative_crits_information(crit_failures)
+        crit_failures_str = negative_text(crit_failures)
         embed.add_field(name='critical failures',
                         value=crit_failures_str, inline=False)
 
     # either all success or the balanced
     if not bot:
-        sucess_str = success_general(sucess)
+        sucess_str = positive_text(sucess)
         embed.add_field(name='success', value=sucess_str, inline=False)
 
-        crits_str = critical_hits_information(crits)
+        crits_str = positive_text(crits)
         embed.add_field(name='critical hits', value=crits_str, inline=False)
     else:
-        sucess_str = success_general(sucess)
+        sucess_str = positive_text(sucess)
         embed.add_field(name='success', value=sucess_str, inline=False)
 
-        balance_str = balance_success_information(balance)
+        balance_str = positive_text(balance)
         embed.add_field(name='balance', value=balance_str, inline=False)
 
     return embed
-
-
-def general_information(general):
-    return str("""```yaml\n""") + general + """ ```"""
-
-
-def success_general(success):
-    return str("""```diff\n+""") + success + """ ```"""
-
-
-def balance_success_information(balance):
-    return str("""```diff\n+""") + balance + """ ```"""
-
-
-def negative_crits_information(negative_crits):
-    return str("""```diff\n-""") + negative_crits + """ ```"""
-
-
-def critical_hits_information(crits):
-    return str("""```diff\n+""") + crits + """ ```"""
 
 
 def create_base_embed():
@@ -79,8 +59,40 @@ def create_base_embed():
     return embed
 
 
-def create_simpler_embed():
+def create_result_success_embed():
     embed = discord.Embed(
         title="Dices results",
+        colour=discord.Color.blue(),
         type='rich')
+    embed.set_footer(text='Success result')
     return embed
+
+
+def create_result_failure_embed():
+    embed = discord.Embed(
+        title="Dices results",
+        colour=discord.Color.dark_grey(),
+        type='rich')
+    embed.set_footer(text='Fail result')
+    return embed
+
+
+def create_critical_failure_embed():
+    embed = discord.Embed(
+        title="Dices results",
+        colour=discord.Color.red(),
+        type='rich')
+    embed.set_footer(text='Critical Fail result')
+    return embed
+
+
+def neutral_text(general):
+    return str("""```yaml\n""") + general + """ ```"""
+
+
+def positive_text(success):
+    return str("""```diff\n+""") + success + """ ```"""
+
+
+def negative_text(negative_crits):
+    return str("""```diff\n-""") + negative_crits + """ ```"""
