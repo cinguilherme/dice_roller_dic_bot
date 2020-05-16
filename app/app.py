1f59eba40247a5c34a07b2c7977780534ee7febd
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 14:05:39 -0300

    add command to clear bot messages

--- app/app.py
+++ app/app.py
@@ -28,6 +28,26 @@ async def reload(ctx, ext):
     await load(ctx, ext)
 
 
+@client.command(pass_context=True, aliases=['cls'],
+                help=("clear N to clear the last N messages from this bot, \
+                        this command will not be able to delete messages that \
+                        are old into this channel"),
+                brief='clear N to clear the last N messages from this bot')
+async def clear(ctx, ammount=100):
+    channel = ctx.message.channel
+    messages = []
+    count = 0
+    async for message in channel.history(limit=100):
+        if message.author.name.find('Dice Roller') != -1:
+            if count == ammount:
+                break
+            print(f'message to be deleted {message}')
+            count += 1
+            messages.append(message)
+
+    await channel.delete_messages(messages)
+
+
 def load_cogs_startup():
     # preload my cogs at start
     for filename in os.listdir('./app/cogs'):
