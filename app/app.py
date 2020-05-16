67a4b5f394f897ef101cc1ebcbfe1afa61fc25f0
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 14:57:40 -0300

    added command to clear commands messages, require some privilleges

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 import os
 
 from discord.ext import commands
+from discord.ext.commands import has_permissions
+
 
 client = commands.Bot(command_prefix='.')
 
@@ -44,6 +46,31 @@ async def clear(ctx, ammount=100):
             print(f'message to be deleted {message}')
             count += 1
             messages.append(message)
+
+    await channel.delete_messages(messages)
+
+
+@client.command(pass_context=True, aliases=['clsc'],
+                help=("clear N to clear the last N messages commandfs for this bot, \
+                        this command will not be able to delete messages that \
+                        are old into this channel"),
+                brief='clear N to clear the last N commands sent for this bot')
+@has_permissions(administrator=True, manage_messages=True, manage_roles=True)
+async def clear_commands_messages(ctx, ammount=100):
+    channel = ctx.message.channel
+
+    commands_list = client.all_commands.keys()
+
+    def check_command(y): return len(list(filter(
+        lambda x: y.startswith(f'.{x}'), commands_list)))
+
+    print(commands_list)
+    messages = []
+
+    async for message in channel.history(limit=100):
+        if message.content.startswith('.'):
+            if check_command(message.content) > 0:
+                messages.append(message)
 
     await channel.delete_messages(messages)
 
