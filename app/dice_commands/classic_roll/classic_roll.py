d5efd84051b4ff44f4d68e59909f63dbb90b1e06
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-16 11:26:43 -0300

    linting fix

--- app/dice_commands/classic_roll/classic_roll.py
+++ app/dice_commands/classic_roll/classic_roll.py
@@ -1,10 +1,10 @@
 
-def roll_damage(dice_pars, roll_functions, simple_output=False):
+def roll_damage(dice_pars, roll_f, simple_output=False):
 
-    number_dices, type_dice, dificulty = roll_functions.interpret_inp(
+    number_dices, type_dice, dificulty = roll_f.interpret_inp(
         dice_pars).values()
 
-    all_dices, success, crit_success, crit_failures = roll_functions.build_results(
+    all_dices, success, crit_success, crit_failures = roll_f.build_results(
         number_dices, type_dice, dificulty).values()
 
     message_object = build_messages_of_damage(
@@ -13,19 +13,24 @@ def roll_damage(dice_pars, roll_functions, simple_output=False):
     return message_object
 
 
-def build_messages_of_damage(all_dices, success, crit_success, dificulty, simple_output=False):
+def build_messages_of_damage(all_dices, success, crit_success,
+                             dificulty, simple_output=False):
+
+    simple = len(success)
+    crits = len(crit_success) + len(success)
+    gen = f"all dices {all_dices} with difficulty of {dificulty}"
 
     if simple_output:
         messages = {
-            'general': (f"all dices {all_dices} with difficulty of {dificulty}"),
-            'simple_output': (f"results {len(success) - len(crit_failures)}, with crits {success_calc} "),
+            'general': gen,
+            'simple_output': (f"results {simple} with crits {crits} "),
         }
         return messages
 
     messages = {
         'general': f'all dices {all_dices}',
         'success': f'results {len(success)} of success is {success}',
-        'crits': (f'results {len(success)+len(crit_success)} of with expecialization'),
+        'crits': (f'results {crits} of with expecialization'),
         'dificulty': f'{dificulty}'
     }
 
@@ -37,34 +42,37 @@ def roll_dices(dices, roll_functions, simple_output=False):
     number_dices, type_dice, dificulty = roll_functions.interpret_inp(
         dices).values()
 
-    all_dices, success, crit_success, crit_failures = roll_functions.build_results(
+    all_dices, success, crits, fails = roll_functions.build_results(
         number_dices, type_dice, dificulty).values()
 
     success_calc = roll_functions.critical_balance(
-        success, crit_success, crit_failures)
+        success, crits, fails)
 
     messages_object = build_messages_of_atack(
-        all_dices, success, crit_success, crit_failures,
+        all_dices, success, crits, fails,
         success_calc, dificulty, simple_output)
 
     return messages_object
 
 
-def build_messages_of_atack(all_dices, success, crit_success, crit_failures,
+def build_messages_of_atack(all_dices, success, crits, fails,
                             success_calc, dificulty, simple_output=False):
 
+    gen = f"all dices {all_dices} with difficulty of {dificulty}"
+    bal = len(success) - len(fails)
+    smp = f"results {bal}, with crits {success_calc} "
     if simple_output:
         messages = {
-            'general': (f"all dices {all_dices} with difficulty of {dificulty}"),
-            'simple_output': (f"results {len(success) - len(crit_failures)}, with crits {success_calc} "),
+            'general': gen,
+            'simple_output': smp,
         }
         return messages
 
     messages = {
         'general': f'all dices {all_dices}',
-        'success': f'results {len(success) - len(crit_failures)} of success {success}',
-        'crits': f'results {len(success)} of successwith expecialization {crit_success}',
-        'fails': f'you had {len(crit_failures)} critical failures - {crit_failures}',
+        'success': f'results {bal} of success {success}',
+        'crits': f'results {len(success)} of with expecialization {crits}',
+        'fails': f'you had {len(fails)} critical failures - {fails}',
         'balance': f'results {success_calc} of success with expecialization',
         'dificulty': f'{dificulty}'
     }
